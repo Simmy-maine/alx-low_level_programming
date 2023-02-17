4bc8472306d5a6f133100c616e4c228aa95f8f7a
mosima <mosimatala123@gmail.com>
2023-02-17 12:12:43 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-prints.c
@@ -0,0 +1,10 @@
+#include <sttdio.h>
+/**
+ * main - A program that prints line with with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
